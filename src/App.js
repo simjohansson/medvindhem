297a3086ead3ce5c786d9980c7468d6b353e1195
simjohansson <simjohansson@gmail.com>
2016-08-10 22:00:40 +0200

    Added react-google-maps
    Added map

--- src/App.js
+++ src/App.js
@@ -1,21 +1,87 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  default as React,
+  Component,
+} from "react";
 
-class App extends Component {
+import update from "react-addons-update";
+
+import Map from "./Map";
+
+export default class App extends Component {
+  state = {
+    markers: [{
+      position: {
+        lat: 25.0112183,
+        lng: 121.52067570000001,
+      },
+      key: `Taiwan`,
+      defaultAnimation: 2,
+    }],
+  };
+
+  handleMapClick = this.handleMapClick.bind(this);
+  handleMarkerRightclick = this.handleMarkerRightclick.bind(this);
+
+  componentDidMount() {
+    setTimeout(() => {
+      let { markers } = this.state;
+      markers = update(markers, {
+        $push: [
+          {
+            position: {
+              lat: 25.99,
+              lng: 122.9,
+            },
+            defaultAnimation: 2,
+            key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
+          },
+        ],
+      });
+      this.setState({ markers });
+    }, 2000);
+  }
+
+  /*
+   * This is called when you click on the map.
+   * Go and try click now.
+   */
+  handleMapClick(event) {
+    let { markers } = this.state;
+    markers = update(markers, {
+      $push: [
+        {
+          position: event.latLng,
+          defaultAnimation: 2,
+          key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
+        },
+      ],
+    });
+    this.setState({ markers });
+  }
+
+  handleMarkerRightclick(index, event) {
+    /*
+     * All you modify is data, and the view is driven by data.
+     * This is so called data-driven-development. (And yes, it's now in
+     * web front end and even with google maps API.)
+     */
+    let { markers } = this.state;
+    markers = update(markers, {
+      $splice: [
+        [index, 1],
+      ],
+    });
+    this.setState({ markers });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Välkommen till MedVind</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      
+      <Map
+        markers={this.state.markers}
+        onMapClick={this.handleMapClick}
+        onMarkerRightclick={this.handleMarkerRightclick}
+      />
     );
   }
 }
-
-export default App;
