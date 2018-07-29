f894e40a58ae54e773a51bd7c533629ac254df12
simjohansson <simjohansson@gmail.com>
2018-07-29 20:23:45 +0200

    Restart medvindhem web

--- src/App.js
+++ src/App.js
@@ -1,87 +1,30 @@
-import {
-  default as React,
-  Component,
-} from "react";
+import React, { Component } from 'react';
+import './App.css';
+import { PlacesWithStandaloneSearchBox } from './SearchBox';
 
-import update from "react-addons-update";
-
-import Map from "./Map";
-
-export default class App extends Component {
-  state = {
-    markers: [{
-      position: {
-        lat: 25.0112183,
-        lng: 121.52067570000001,
-      },
-      key: `Taiwan`,
-      defaultAnimation: 2,
-    }],
-  };
-
-  handleMapClick = this.handleMapClick.bind(this);
-  handleMarkerRightclick = this.handleMarkerRightclick.bind(this);
-
-  componentDidMount() {
-    setTimeout(() => {
-      let { markers } = this.state;
-      markers = update(markers, {
-        $push: [
-          {
-            position: {
-              lat: 25.99,
-              lng: 122.9,
-            },
-            defaultAnimation: 2,
-            key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
-          },
-        ],
-      });
-      this.setState({ markers });
-    }, 2000);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this.onPlacesChanged = this.onPlacesChanged.bind(this);
   }
 
-  /*
-   * This is called when you click on the map.
-   * Go and try click now.
-   */
-  handleMapClick(event) {
-    let { markers } = this.state;
-    markers = update(markers, {
-      $push: [
-        {
-          position: event.latLng,
-          defaultAnimation: 2,
-          key: Date.now(), // Add a key property for: http://fb.me/react-warning-keys
-        },
-      ],
-    });
-    this.setState({ markers });
-  }
-
-  handleMarkerRightclick(index, event) {
-    /*
-     * All you modify is data, and the view is driven by data.
-     * This is so called data-driven-development. (And yes, it's now in
-     * web front end and even with google maps API.)
-     */
-    let { markers } = this.state;
-    markers = update(markers, {
-      $splice: [
-        [index, 1],
-      ],
-    });
-    this.setState({ markers });
-  }
 
   render() {
     return (
-      
-      <Map
-        markers={this.state.markers}
-        onMapClick={this.handleMapClick}
-        onMarkerRightclick={this.handleMarkerRightclick}
-      />
+    <React.Fragment>
+      <PlacesWithStandaloneSearchBox placesChanged={this.onPlacesChanged} />
+      <PlacesWithStandaloneSearchBox placesChanged={this.onPlacesChanged} />
+    </React.Fragment>
     );
   }
+
+  onPlacesChanged(places) {
+    console.log(places[0].formatted_address);
+  }
+
 }
+
+export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,65 @@
+import { compose, withProps, lifecycle } from "recompose";
+import React from 'react';
+import { StandaloneSearchBox } from "react-google-maps/lib/components/places/StandaloneSearchBox";
+
+export const PlacesWithStandaloneSearchBox =  compose(
+    withProps({
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />
+    }),
+    lifecycle({
+        componentWillMount() {
+            const refs = {}
+
+            this.setState({
+                places: [],
+                onSearchBoxMounted: ref => {
+                    refs.searchBox = ref;
+                },
+                onPlacesChanged: () => {
+                    const places = refs.searchBox.getPlaces();
+                    debugger;
+                    refs.searchBox.props.callBack(places);
+                    this.setState({
+                        places,
+                    });
+                },
+            })
+        },
+    })
+)(props =>
+    <div data-standalone-searchbox="">
+        <StandaloneSearchBox
+            ref={props.onSearchBoxMounted}
+            bounds={props.bounds}
+            onPlacesChanged={props.onPlacesChanged}
+            callBack={props.placesChanged}
+        >
+            <input
+                type="text"
+                placeholder="Customized your placeholder"
+                style={{
+                    boxSizing: `border-box`,
+                    border: `1px solid transparent`,
+                    width: `240px`,
+                    height: `32px`,
+                    padding: `0 12px`,
+                    borderRadius: `3px`,
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `14px`,
+                    outline: `none`,
+                    textOverflow: `ellipses`,
+                }}
+            />
+        </StandaloneSearchBox>
+        <ol>
+            {props.places.map(({ place_id, formatted_address, geometry: { location } }) =>
+                <li key={place_id}>
+                    {formatted_address}
+                    {" at "}
+                    ({location.lat()}, {location.lng()})
+        </li>
+            )}
+        </ol>
+    </div>
+);
